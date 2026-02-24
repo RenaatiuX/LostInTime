4f3b79bfcafb220240676208ddfb25915846bc75
Bretzelfresser <bretzelfresser@gmail.com>
2026-02-24 11:20:27 +0100

    gemini added all the identification recipes

--- src/main/java/com/ren/lostintime/datagen/server/LITRecipeProvider.java
+++ src/main/java/com/ren/lostintime/datagen/server/LITRecipeProvider.java
@@ -17,6 +17,7 @@ import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class LITRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -29,13 +30,270 @@ public class LITRecipeProvider extends RecipeProvider implements IConditionBuild
     protected void buildRecipes(@NotNull Consumer<FinishedRecipe> pWriter) {
         cookingFood("cooked_dodo", ItemInit.RAW_DODO.get(), ItemInit.COOKED_DODO.get(),0.35F, pWriter);
 
-        identify(ItemInit.QUATERNARY_FOSSIL.get()).addOutput(ItemInit.DODO_FOSSIL.get(), 25)
-                .addOutput(ItemInit.DODO_SKULL.get(), 5).addOutput(Items.BONE, 33)
-                .addOutput(Items.BONE_MEAL, 33).addOutput(Items.NAUTILUS_SHELL, 1)
-                .addOutput(ItemInit.AMBER.get(), 0.25D).addOutput(Items.COBBLESTONE, 15)
-                .addOutput(Items.COBBLED_DEEPSLATE, 30).addOutput(Items.GRAVEL, 33)
-                .addOutput(Items.IRON_NUGGET, 1, 3, 10).addOutput(Items.GOLD_NUGGET, 1, 3, 0.25)
-                .addOutput(Items.DIRT, 25).addOutput(Items.SKELETON_SKULL, 1).save(pWriter);
+        // Quaternary
+        addFossilRecipe(pWriter, ItemInit.QUATERNARY_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.DODO_FOSSIL.get(), 25)
+                .addOutput(ItemInit.DODO_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(Items.SKELETON_SKULL, 1)
+                .addOutput(Items.NAUTILUS_SHELL, 1)
+                .addOutput(ItemInit.AMBER.get(), 0.25)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Neogene
+        addFossilRecipe(pWriter, ItemInit.NEOGENE_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.DAEODON_FOSSIL.get(), 20)
+                .addOutput(ItemInit.DAEODON_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.25)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Paleogene
+        addFossilRecipe(pWriter, ItemInit.PALEOGENE_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.LEPTICTIDIUM_FOSSIL.get(), 25)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.25)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Cretaceous
+        addFossilRecipe(pWriter, ItemInit.CRETACEOUS_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.DEINONYCHUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.DEINONYCHUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.33)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Jurassic
+        addFossilRecipe(pWriter, ItemInit.JURASSIC_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.PLESIOSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.PLESIOSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.4)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_JURASSIC_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.PLESIOSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.PLESIOSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.4)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Triassic
+        addFossilRecipe(pWriter, ItemInit.TRIASSIC_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.MASTODONSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.MASTODONSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.125)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_TRIASSIC_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.MASTODONSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.MASTODONSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.125)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 1.25)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 1.25)
+        );
+
+        // Permian
+        addFossilRecipe(pWriter, ItemInit.PERMIAN_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.SCUTOSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.SCUTOSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.125)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_PERMIAN_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.SCUTOSAURUS_FOSSIL.get(), 25)
+                .addOutput(ItemInit.SCUTOSAURUS_SKULL.get(), 5)
+                .addOutput(Items.BONE, 33)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.125)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+
+        // Carboniferous
+        addFossilRecipe(pWriter, ItemInit.CARBONIFEROUS_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.HYLONOMUS_FOSSIL.get(), 25)
+                .addOutput(Items.BONE, 15)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.25)
+                .addOutput(Items.COAL, 20)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.08)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_CARBONIFEROUS_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.HYLONOMUS_FOSSIL.get(), 25)
+                .addOutput(Items.BONE, 15)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(ItemInit.AMBER.get(), 0.25)
+                .addOutput(Items.COAL, 20)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.08)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.08)
+        );
+
+        // Devonian
+        addFossilRecipe(pWriter, ItemInit.DEVONIAN_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.BOTHRIOLEPIS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE, 15)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_DEVONIAN_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.BOTHRIOLEPIS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE, 15)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+
+        // Silurian
+        addFossilRecipe(pWriter, ItemInit.SILURIAN_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.PTERYGOTUS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(Items.CALCITE, 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_SILURIAN_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.PTERYGOTUS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(Items.CALCITE, 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+
+        // Ordovician
+        addFossilRecipe(pWriter, ItemInit.ORDOVICIAN_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.ENDOCERAS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(Items.CALCITE, 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_ORDOVICIAN_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.ENDOCERAS_FOSSIL.get(), 20)
+                .addOutput(Items.BONE_MEAL, 33)
+                .addOutput(Items.CALCITE, 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+
+        // Cambrian
+        addFossilRecipe(pWriter, ItemInit.CAMBRIAN_FOSSIL.get(), false, builder -> builder
+                .addOutput(ItemInit.ANOMALOCARIS_FOSSIL.get(), 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
+        addFossilRecipe(pWriter, BlockInit.DEEPSLATE_CAMBRIAN_FOSSIL_BLOCK.get(), true, builder -> builder
+                .addOutput(ItemInit.ANOMALOCARIS_FOSSIL.get(), 10)
+                .addOutput(BlockInit.DEAD_BARREL_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_GLASS_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_PIPE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_TREE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_VASE_SPONGE.get(), 2.5)
+                .addOutput(BlockInit.DEAD_WOOL_SPONGE.get(), 2.5)
+        );
 
         soulExtract(ItemInit.ASPECT_EMERGENCE.get())
                 .addInput(Ingredient.of(ItemInit.SOUL_GRUME.get()))
@@ -59,6 +317,26 @@ public class LITRecipeProvider extends RecipeProvider implements IConditionBuild
                 .input(ItemInit.DODO_SOUL_CFC.get())
                 .unlockedBy("hasItem", has(ItemInit.DODO_SOUL_CFC.get())).save(pWriter);
 
+    }
+
+    private void addFossilRecipe(Consumer<FinishedRecipe> pWriter, ItemLike input, boolean isDeepslate, Consumer<IdentificationBuilder> builderConsumer) {
+        IdentificationBuilder builder = identify(input);
+        
+        // Common outputs
+        if (isDeepslate) {
+            builder.addOutput(Items.COBBLED_DEEPSLATE, 30);
+        } else {
+            builder.addOutput(Items.COBBLESTONE, 15);
+        }
+        builder.addOutput(Items.GRAVEL, 33);
+        builder.addOutput(Items.IRON_NUGGET, 1, 3, 10);
+        builder.addOutput(Items.GOLD_NUGGET, 1, 2, 2.5);
+        builder.addOutput(Items.DIRT, 25);
+
+        // Specific outputs
+        builderConsumer.accept(builder);
+        
+        builder.save(pWriter);
     }
 
     private static void cookingFood(String name, ItemLike ingredient, ItemLike result, float experience, Consumer<FinishedRecipe> consumer) {
